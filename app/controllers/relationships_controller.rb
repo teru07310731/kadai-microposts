42de145000943c8eb601a4c1470593d9a66ebe1d
teruteru07310731 <40971412+teru07310731@users.noreply.github.com>
2018-10-14 04:35:06 +0000

    follow, unollow

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,17 +1,7 @@
 class RelationshipsController < ApplicationController
-  before_action :require_user_logged_in
-
   def create
-    user = User.find(params[:follow_id])
-    current_user.follow(user)
-    flash[:success] = 'ユーザをフォローしました。'
-    redirect_to user
   end
 
   def destroy
-    user = User.find(params[:follow_id])
-    currser)
-    flash[:success] =ent_user.unfollow(u 'ユーザのフォローを解除しました。'
-    redirect_to user
   end
 end
--- db/migrate/20181013161217_create_relationships.rb
+++ db/migrate/20181013161217_create_relationships.rb
@@ -2,7 +2,7 @@ class CreateRelationships < ActiveRecord::Migration[5.0]
   def change
     create_table :relationships do |t|
       t.references :user, foreign_key: true
-      t.references :follow, foreign_key: true
+      t.references :follow, foreign_key: { to_table: :users }
 
       t.timestamps
 
