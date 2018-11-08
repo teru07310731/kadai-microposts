6298fe696eb467cd42aab83d9a0e78b05b475840
teruteru07310731 <40971412+teru07310731@users.noreply.github.com>
2018-11-08 09:41:54 +0000

    45

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   
   has_many :microposts
   has_many :relationships
-  has_many:followings, through: :relationships, source: :follow
+  has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
 
